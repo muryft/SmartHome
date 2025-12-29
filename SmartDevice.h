976a4996c0dbd4da9de0207c13a79d8ce0686d40
muryft <yagmurytuncer@gmail.com>
2025-12-29 16:47:51 +0300

    Update SmartDevice.h

--- SmartDevice.h
+++ SmartDevice.h
@@ -1,1 +1,17 @@
+class SmartDevice {
+protected:
+    string id;
+    bool isOn;
+
+public:
+    SmartDevice(string id) : id(id), isOn(false) {}
+    virtual ~SmartDevice() {}
+
+    string getId() const { return id; }
+
+    virtual void turnOn()  { isOn = true; }
+    virtual void turnOff() { isOn = false; }
+
+    virtual void showStatus() const = 0;
+};
 
